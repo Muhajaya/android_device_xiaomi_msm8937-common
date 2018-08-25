610804e802d1e47483a4a72347533433c5cb8334
joshuous <joshuous@gmail.com>
2018-08-25 07:52:27 +0000

    msm8937-common: Set DISABLE_EAP_PROXY to break wpa_supplicant dependency on qmi
    Without this flag, wpa_supplicant fails to build because we do not have
    sources for these proprietary libs: libqmi_cci, libqmiservices, libidl.
    
    Reference:
    https://source.codeaurora.org/quic/la/platform/external/wpa_supplicant_8/commit/?h=cfd8fe3d57bcf0976dddb0d82ec04fcd2dbc5ebc
    
    Change-Id: Id161eadd9e5327768d1f41fb621d4422aeaa27ec
    Signed-off-by: joshuous <joshuous@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -221,6 +221,8 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+# Disable qmi EAP-SIM security
+DISABLE_EAP_PROXY := true
 
 # Inherit the common proprietary files
 -include vendor/xiaomi/msm8937-common/BoardConfigVendor.mk
